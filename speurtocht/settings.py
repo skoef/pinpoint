3abad6208368af9445ca24d9b1662d90e7a007c9
Reinier Schoof <reinier@skoef.nl>
2026-09-04 11:05:48 +0200

    fix: allow to set CRSF trusted origins

--- speurtocht/settings.py
+++ speurtocht/settings.py
@@ -24,6 +24,8 @@ DEBUG = os.environ.get("DEBUG", "true").lower() == "true"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost 127.0.0.1").split()
 
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "").split()
+
 
 # Application definition
 
